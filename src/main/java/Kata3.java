b5baf30a2f32d9a4da196763ca33606f55d54d35
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-01-30 16:33:05 +0100

    another short session

---
+++ src/main/java/Kata3.java
@@ -0,0 +1,14 @@
+public class Kata3 {
+    public static String HighAndLow(String numbers) {
+        String[] numbersSA = numbers.split(" ");
+        Integer max = Integer.MIN_VALUE;
+        Integer min = Integer.MAX_VALUE;
+
+        for (String s : numbersSA) {
+            Integer currentValue = Integer.valueOf(s);
+            if (currentValue > max) max = currentValue;
+            if (currentValue < min) min = currentValue;
+        }
+        return max.toString() + " " + min.toString();
+    }
+}
