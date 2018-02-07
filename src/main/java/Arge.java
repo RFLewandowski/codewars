3ffa0ed79435fb3ca7aa67cd3e3303689d90bc29
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-02-07 20:37:26 +0100

    another short session

---
+++ src/main/java/Arge.java
@@ -0,0 +1,12 @@
+class Arge {
+
+    public static int nbYear(int p0, double percent, int aug, int p) {
+
+        int currentPop = p0;
+        int year = 0;
+        for (; currentPop <= p; year++) {
+            currentPop +=(int) (((currentPop * percent) / 100) + aug);
+        }
+        return year;
+    }
+}
