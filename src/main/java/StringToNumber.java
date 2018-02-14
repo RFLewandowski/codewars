653720055febbd907e707461dd371e07688c7af6
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-02-14 20:30:14 +0100

    another short session

---
+++ src/main/java/StringToNumber.java
@@ -0,0 +1,6 @@
+public class StringToNumber {
+    public static int stringToNumber(String str) {
+        System.out.println(str.substring(1,str.length()-1));
+        return Integer.decode(str);
+    }
+}
