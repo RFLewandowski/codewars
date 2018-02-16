794b7efddb7873db0188c44f5669f5fb3084c2c2
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-02-16 18:05:14 +0100

    another short session

--- src/main/java/Kata9.java
+++ src/main/java/Kata9.java
@@ -2,7 +2,6 @@ import java.util.Arrays;
 
 public class Kata9 {
     public static String findNeedle(Object[] haystack) {
-
         return "found the needle at position " + Arrays.asList(haystack).indexOf("needle");
 //
 //        for (int i = 0; i < haystack.length; i++) {
