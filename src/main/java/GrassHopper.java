593da58bcfd75d89e731adb68bb0468757d65511
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-02-18 18:55:11 +0100

    another short session

--- src/main/java/GrassHopper.java
+++ src/main/java/GrassHopper.java
@@ -1,10 +1,14 @@
+import java.util.stream.IntStream;
+
 public class GrassHopper {
 
     public static int summation(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += i;
-        }
-        return sum;
+
+        return IntStream.rangeClosed(1,n).sum();
+//        int sum = 0;
+//        for (int i = 1; i <= n; i++) {
+//            sum += i;
+//        }
+//        return sum;
     }
 }
