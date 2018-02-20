4d476942a4729762e0f1383a5477b2247814d157
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-02-20 19:51:03 +0100

    another short session

---
+++ src/main/java/Kata0.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Kata0 {
+    public int min(int[] list) {
+        return Arrays
+                .stream(list)
+                .min()
+                .getAsInt();
+    }
+
+    public int max(int[] list) {
+        return Arrays
+                .stream(list)
+                .max()
+                .getAsInt();
+    }
+}
