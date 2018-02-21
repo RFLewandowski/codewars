1bf4d1ee83c7591bd4165917208f3ce1ca8249b1
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-02-21 19:35:04 +0100

    another short session

---
+++ src/main/java/Kata10.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class Kata10 {
+    public static int sum(int[] numbers) {
+        if (numbers == null || numbers.length <= 1) return 0;
+        int min = Arrays.stream(numbers).min().getAsInt();
+        int max = Arrays.stream(numbers).max().getAsInt();
+        return Arrays.stream(numbers).sum() - min - max;
+    }
+}
