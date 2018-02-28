61e7c2027bf800800e2363ccd97f1fef19f776fa
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-02-28 21:18:03 +0100

    another short session

---
+++ src/main/java/Kata2.java
@@ -0,0 +1,11 @@
+public class Kata2 {
+    public static int[] invert(int[] array) {
+            return java.util.Arrays.stream(array).map(i -> -i).toArray();
+//        int length = array.length;
+//        int[] inverted = new int[length];
+//        for (int i = 0; i < length; i++) {
+//            inverted[i] = -array[i];
+//        }
+//        return inverted;
+    }
+}
