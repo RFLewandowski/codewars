398e0fda96d9c89ddd63fa5806364bb98e3d787c
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-03-05 20:35:11 +0100

    another short session

---
+++ src/main/java/Kata5.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Kata5 {
+    public static List CreateList(int number) {
+        List listSerie = new ArrayList();
+        for (int count = 1; count <= number; count++) {
+            listSerie.add(count);
+        }
+        return listSerie;
+    }
+}
