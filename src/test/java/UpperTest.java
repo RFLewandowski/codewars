c0660061b762a2390ad85acf95aae6064e2bd779
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-03-14 19:39:08 +0100

    another short session

---
+++ src/test/java/UpperTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class UpperTest {
+    @Test
+    public void testSomething() {
+        assertEquals("HELLO", Upper.MakeUpperCase("hello"));
+    }
+}
