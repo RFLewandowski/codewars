941c80118bb51e7c73d6518ce30ef65733af2780
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-03-15 19:58:00 +0100

    another short session

---
+++ src/main/java/Kata9.java
@@ -0,0 +1,8 @@
+public class Kata9 {
+    private final static int COST_PER_DAY = 40;
+    public static int rentalCarCost(int d) {
+        if (d >= 7) return d * COST_PER_DAY - 50;
+        else if (d >= 3) return d * COST_PER_DAY - 20;
+        return d * COST_PER_DAY;
+    }
+}
---
+++ src/test/java/Kata9Test.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Kata9Test {
+    @Test
+    public void under3Tests() {
+        assertEquals(40, Kata9.rentalCarCost(1));
+        assertEquals(80, Kata9.rentalCarCost(2));
+    }
+
+    @Test
+    public void under7Tests() {
+        assertEquals(100, Kata9.rentalCarCost(3));
+        assertEquals(140, Kata9.rentalCarCost(4));
+        assertEquals(180, Kata9.rentalCarCost(5));
+        assertEquals(220, Kata9.rentalCarCost(6));
+    }
+
+    @Test
+    public void over7Tests() {
+        assertEquals(230, Kata9.rentalCarCost(7));
+        assertEquals(270, Kata9.rentalCarCost(8));
+        assertEquals(310, Kata9.rentalCarCost(9));
+        assertEquals(350, Kata9.rentalCarCost(10));
+    }
+}
