00562454caf79503c3ce55f68dfe00eb5ee9bce1
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-03-23 17:29:11 +0100

    another short session

---
+++ src/test/java/Kata1Test.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Kata1Test {
+    @Test
+    public void tests() {
+        assertEquals(true, Kata1.betterThanAverage(new int[]{2, 3}, 5));
+        assertEquals(true, Kata1.betterThanAverage(new int[]{100, 40, 34, 57, 29, 72, 57, 88}, 75));
+        assertEquals(true, Kata1.betterThanAverage(new int[]{12, 23, 34, 45, 56, 67, 78, 89, 90}, 69));
+        assertEquals(false, Kata1.betterThanAverage(new int[]{100, 90}, 11));
+    }
+}
