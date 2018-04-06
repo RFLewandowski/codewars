d38620035c658c51ef6f79763f1ea34b587d6bea
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-04-06 22:46:39 +0200

    another short session

---
+++ src/main/java/NameMe.java
@@ -0,0 +1,23 @@
+public class NameMe {
+    private String firstName;
+    private String lastName;
+    private String fullName;
+
+    public NameMe(String first, String last) {
+        this.firstName = first;
+        this.lastName = last;
+        this.fullName = first + " " + last;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName() {
+        return this.fullName;
+    }
+}
