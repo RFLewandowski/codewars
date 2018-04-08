385f156f2e9df22021ec71f54e136c441f222b51
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-04-08 10:48:09 +0200

    another short session

---
+++ src/test/java/ClockTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class ClockTest {
+    @Test
+    public void test1() {
+        assertEquals(Clock.Past(0, 1, 1), 61000);
+    }
+}
