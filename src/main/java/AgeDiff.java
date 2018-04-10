c5d16d248931d1412da22af506eba305fa216127
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-04-10 18:46:57 +0200

    another short session

---
+++ src/main/java/AgeDiff.java
@@ -0,0 +1,10 @@
+public class AgeDiff {
+    public static String CalculateAge(int birth, int yearTo) {
+        int age = yearTo - birth;
+        if (age > 1) return "You are " + age + " years old.";
+        if (age == 1) return "You are 1 year old.";
+        if (age == 0) return "You were born this very year!";
+        if (age == -1) return "You will be born in 1 year.";
+        return "You will be born in " + -age + " years.";
+    }
+}
