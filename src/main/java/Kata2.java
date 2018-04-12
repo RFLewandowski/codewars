26a0cd78ff57c0a49580ece5053e6606b56b4545
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-04-12 16:31:13 +0200

    another short session

---
+++ src/main/java/Kata2.java
@@ -0,0 +1,7 @@
+public class Kata2 {
+    public static int[] getSize(int w, int h, int d) {
+        int volume = w * h * d;
+        int surface = 2 * (w * h + h * d + d * w);
+        return new int[]{surface, volume};
+    }
+}
