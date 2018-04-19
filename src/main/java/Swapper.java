896d39f54ad55d95911d58ae3fcd4de693247c70
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-04-19 21:38:03 +0200

    another short session

---
+++ src/main/java/Swapper.java
@@ -0,0 +1,18 @@
+class Swapper {
+
+    private Object[] arguments;
+
+    public Swapper(final Object[] args) {
+        arguments = args;
+    }
+
+    public void swapValues() {
+        Object temp = arguments[0];
+        arguments[0] = arguments[1];
+        arguments[1] = temp;
+    }
+
+    public Object[] getArguments() {
+        return arguments;
+    }
+}
