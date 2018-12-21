d9dfaa83a2fd4e5b29abef5e83a7e8efbcb1dc6e
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-12-21 23:42:55 +0100

    checking chars method - stream of characters from string

---
+++ src/main/java/Competition.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+
+public class Competition {
+    public static void main(String[] args) {
+        Arrays.stream(new Byte[] {21, 0, 21, 17}).map(b -> (char) (b + 'a')).forEach(System.out::print);
+    }
+}
